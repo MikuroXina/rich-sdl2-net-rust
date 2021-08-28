b7d8eb6139b8130169a44d6def7fccfa48e8cef0
MikuroXina <ryosukadnak@gmail.com>
2021-08-29 02:00:15 +0900

    feat: Add Net

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,34 @@
+//! # rich-sdl2-net-rust
+//!
+//! The SDL_net 2.0 wrapper for Rust.
+
+#![warn(missing_docs)]
+
+use rich_sdl2_rust::Sdl;
+use std::marker::PhantomData;
+
 mod bind;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+/// A root controller for SDL2_net.
+pub struct Net<'sdl> {
+    _phantom: PhantomData<&'sdl Sdl>,
+}
+
+impl<'sdl> Net<'sdl> {
+    /// Constructs a root controller with SDL2 controller.
+    pub fn new(_sdl: &'sdl Sdl) -> Self {
+        let ret = unsafe { bind::SDLNet_Init() };
+        if ret != 0 {
+            Sdl::error_then_panic("sdl_net init");
+        }
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl Drop for Net<'_> {
+    fn drop(&mut self) {
+        unsafe { bind::SDLNet_Quit() }
     }
 }
